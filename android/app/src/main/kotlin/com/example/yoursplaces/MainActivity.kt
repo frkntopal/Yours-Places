f655f725a561ddb87a1bce41b2d81d63a1e9cfbb
Furkan Topal <frkntopall@outlook.com>
2023-01-20 16:09:28 +0300

    I create our pages for clean code

---
+++ android/app/src/main/kotlin/com/example/yoursplaces/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.yoursplaces
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/places_list_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class PlacesListScreen extends StatelessWidget {
+  const PlacesListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
